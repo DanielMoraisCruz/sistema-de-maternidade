26ea1f7084d4e640c70c2ef2e26ed1975df4edc1
Daniel Cruz <daniel.jack.dmc@gmail.com>
2023-01-16 13:54:18 -0300

    att gitiginore

--- coding/testeConexao.py
+++ coding/testeConexao.py
@@ -1,18 +1,21 @@
 import mysql.connector
 
-conexao = mysql.connector.connect(  # funcao que realiza a conexão entre o programa e o BD
-    host='localhost',  # passar o host, se o BD estiver no seu computador use local Host
+conexao = mysql.connector.connect(  # funcao que realiza a conexão
+                                    # entre o programa e o BD
+    host='localhost',  # passar o host, se o BD estiver no seu
+                       # computador use local Host
     user='root',  # Usuario do BD
-    password='3650',  # Senha do BD
+    password='OlljFVAZlRVsdNiTncrS',  # Senha do BD
     database='maternidade',  # Nome do esquema que será alterado
-) 
+)
 
 cursor = conexao.cursor()
 
-#inserindo dados
+# inserindo dados
 crm = "12345"
 especialidade = "parteiro"
 nome = "lucas"
-comando = f'INSERT INTO medico (CRM,Especialidade,Nome) VALUES ("{crm}", "{especialidade}","{nome}")'
+comando = f'''INSERT INTO medico (CRM,Especialidade,Nome)
+              VALUES ("{crm}", "{especialidade}","{nome}")'''
 cursor.execute(comando)
-conexao.commit() # edita o banco de dado
+conexao.commit()  # edita o banco de dado
